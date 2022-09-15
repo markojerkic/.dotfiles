1926618afd9e4486560d515e070824f1258b5015
Marko Jerkić <markojerkic266@gmail.com>
2022-09-15 23:59:09 +0200

    add fuzzy finder

--- Microsoft.Powershell_profile.ps1
+++ Microsoft.Powershell_profile.ps1
@@ -1,3 +1,8 @@
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/larserikfinholt.omp.json" | Invoke-Expression
 Import-Module posh-git
 
+function Invoke-FuzzyEditDev {Invoke-FuzzyEdit ~\Dev}
+Set-Alias -Name fe -Value Invoke-FuzzyEditDev
+function Invoke-FuzzySetLocationDev {Invoke-FuzzySetLocation ~\Dev}
+Set-Alias -Name fs -Value Invoke-FuzzySetLocationDev
+
