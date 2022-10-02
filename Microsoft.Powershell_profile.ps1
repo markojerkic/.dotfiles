f71344059b683f428e4333fc95fd12760eecaa7a
Marko Jerkić <markojerkic266@gmail.com>
2022-10-02 19:02:41 +0200

    Update Microsoft.Powershell_profile.ps1

--- Microsoft.Powershell_profile.ps1
+++ Microsoft.Powershell_profile.ps1
@@ -1,8 +1,13 @@
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/larserikfinholt.omp.json" | Invoke-Expression
 Import-Module posh-git
 
-function Invoke-FuzzyEditDev {Invoke-FuzzyEdit ~\Dev}
+$startLocation = $args[0].ToString().Trim();
+
+function Invoke-FuzzyEditDev { Invoke-FuzzyEdit $startLocation }
 Set-Alias -Name fe -Value Invoke-FuzzyEditDev
-function Invoke-FuzzySetLocationDev {Invoke-FuzzySetLocation ~\Dev}
+
+function Invoke-FuzzySetLocationDev { get-childitem -Path $startLocation -name -depth 3 -Directory | Where-Object { -not($_.contains("\node_modules")) } | Where-Object { -not($_.contains("\.")) } | Where-Object { -not($_.contains("\yarn")) } | Invoke-Fzf | % { "$startLocation\$_" } | Set-Location }
 Set-Alias -Name fs -Value Invoke-FuzzySetLocationDev
 
+function Invoke-PsFzfGitBranchesDev { git branch | Invoke-Fzf | % { $_.replace(" ", "") } | % { $_.replace("*", "") } | % { git switch $_ } }
+Set-Alias -Name fg -Value Invoke-PsFzfGitBranchesDev
