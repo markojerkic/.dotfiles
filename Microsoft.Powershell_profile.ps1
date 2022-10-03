8223ca39af27e7c11241666de6ae0fc8a1227f10
Marko Jerkić <markojerkic266@gmail.com>
2022-10-03 09:52:01 +0200

    Update Microsoft.Powershell_profile.ps1

--- Microsoft.Powershell_profile.ps1
+++ Microsoft.Powershell_profile.ps1
@@ -1,13 +1,36 @@
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/larserikfinholt.omp.json" | Invoke-Expression
 Import-Module posh-git
+Import-Module PSFzf
 
+# Search directory
 $startLocation = $args[0].ToString().Trim();
 
-function Invoke-FuzzyEditDev { Invoke-FuzzyEdit $startLocation }
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+
+# Autocompletion for arrow keys
+Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
+
+function Invoke-FuzzyEditDev {Invoke-FuzzyEdit $startLocation }
 Set-Alias -Name fe -Value Invoke-FuzzyEditDev
 
 function Invoke-FuzzySetLocationDev { get-childitem -Path $startLocation -name -depth 3 -Directory | Where-Object { -not($_.contains("\node_modules")) } | Where-Object { -not($_.contains("\.")) } | Where-Object { -not($_.contains("\yarn")) } | Invoke-Fzf | % { "$startLocation\$_" } | Set-Location }
 Set-Alias -Name fs -Value Invoke-FuzzySetLocationDev
 
-function Invoke-PsFzfGitBranchesDev { git branch | Invoke-Fzf | % { $_.replace(" ", "") } | % { $_.replace("*", "") } | % { git switch $_ } }
-Set-Alias -Name fg -Value Invoke-PsFzfGitBranchesDev
+function Invoke-FuzzyGitSwitch { git branch |  % {$_.replace("*","")} | % {$_.replace(" ","")} | Invoke-Fzf | % {git switch $_} }
+Set-Alias -Name fg -Value Invoke-FuzzyGitSwitch
+
+function Invoke-FuzzyGitSwitchRemote { git branch -r |  % {$_.replace("*","")} | % {$_.replace(" ","")} | Invoke-Fzf | % {git checkout $_ -t } }
+Set-Alias -Name fgr -Value Invoke-FuzzyGitSwitchRemote
+
+function Invoke-FuzzyGitSwitchDelete { git branch |  % {$_.replace("*","")} | % {$_.replace(" ","")} | Invoke-Fzf | % {git branch -d $_ } }
+Set-Alias -Name fgd -Value Invoke-FuzzyGitSwitchDelete 
+
+function Invoke-FuzzyGitSwitchDeleteForce { git branch |  % {$_.replace("*","")} | % {$_.replace(" ","")} | Invoke-Fzf | % {git branch -D $_ } }
+Set-Alias -Name fgdf -Value Invoke-FuzzyGitSwitchDeleteForce
+
+function Invoke-GitSwitch { git switch -c $args[0] }
+Set-Alias -Name gs -Value Invoke-GitSwitch 
+
+function Invoke-GitAddPartial { git add -p . }
+Set-Alias -Name gap -Value Invoke-GitAddPartial 
