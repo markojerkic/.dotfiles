47a25166a7514a5f8040b24fb67743c72bcbb940
Marko Jekrić <markojerkic266@gmail.com>
2022-10-16 20:07:26 +0200

    update posh profile

--- Microsoft.Powershell_profile.ps1
+++ Microsoft.Powershell_profile.ps1
@@ -2,35 +2,31 @@ oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/marcduiker.omp.json" | Invo
 Import-Module posh-git
 Import-Module PSFzf
 
-# Search directory
-$startLocation = $Env:StartLocation.Trim();
-if (-not($startLocation)) {
-    Write-Error "Export StartLocation env variable";
-    exit 1;
-}
-
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
 
 # Autocompletion for arrow keys
 Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
 
-function Invoke-FuzzyEditDev {Invoke-FuzzyEdit $startLocation }
+function Invoke-FuzzyEditDev { Invoke-FuzzyEdit $startLocation }
 Set-Alias -Name fe -Value Invoke-FuzzyEditDev
 
-function Invoke-FuzzySetLocationDev { c:\stribog.exe -u -r "C:\" -f node_modules -f ".git" -f ".next" -f ".mvn" -f ".swc" -f build -f target  -f AppData -f "android" -f "apache-maven-3.8.5" -f "DownLoadiTunes" -f "Drivers" -f "ESD" -f "Log" -f "MinGW" -f "PerfLogs" -f "Program Files" -f "Program Files (x86)" -f "pscale_0.93.0_windows_arm64" -f "Python27" -f "Python39" -f "Riot Games" -f "Ruby26-x64" -f "Windows" -f temp -f ProgramData -f "$" -f dist  | Invoke-fzf | Set-Location }
+function Invoke-FuzzySetLocationDev { 
+    c:\stribog.exe -c -u -r "C:\Dev" -f node_modules -f bin -f ".m2" -f ".vscode" -f ".gradle" -f ".git" -f ".next" -f ".mvn" -f ".swc" -f build -f target  -f AppData  -f dist &
+    c:\stribog.exe -n -u -r "C:\Dev" -f node_modules -f bin -f ".m2" -f ".vscode" -f ".gradle" -f ".git" -f ".next" -f ".mvn" -f ".swc" -f build -f target  -f AppData  -f dist  | Invoke-fzf | Set-Location
+}
 Set-Alias -Name fs -Value Invoke-FuzzySetLocationDev
 
-function Invoke-FuzzyGitSwitch { git branch |  % {$_.replace("*","")} | % {$_.replace(" ","")} | Invoke-Fzf | % {git switch $_} }
+function Invoke-FuzzyGitSwitch { git branch | % { $_.replace("*", "") } | % { $_.replace(" ", "") } | Invoke-Fzf | % { git switch $_ } }
 Set-Alias -Name fg -Value Invoke-FuzzyGitSwitch
 
-function Invoke-FuzzyGitSwitchRemote { git branch -r |  % {$_.replace("*","")} | % {$_.replace(" ","")} | Invoke-Fzf | % {git checkout $_ -t } }
+function Invoke-FuzzyGitSwitchRemote { git branch -r | % { $_.replace("*", "") } | % { $_.replace(" ", "") } | Invoke-Fzf | % { git checkout $_ -t } }
 Set-Alias -Name fgr -Value Invoke-FuzzyGitSwitchRemote
 
-function Invoke-FuzzyGitSwitchDelete { git branch |  % {$_.replace("*","")} | % {$_.replace(" ","")} | Invoke-Fzf | % {git branch -d $_ } }
+function Invoke-FuzzyGitSwitchDelete { git branch | % { $_.replace("*", "") } | % { $_.replace(" ", "") } | Invoke-Fzf | % { git branch -d $_ } }
 Set-Alias -Name fgd -Value Invoke-FuzzyGitSwitchDelete 
 
-function Invoke-FuzzyGitSwitchDeleteForce { git branch |  % {$_.replace("*","")} | % {$_.replace(" ","")} | Invoke-Fzf | % {git branch -D $_ } }
+function Invoke-FuzzyGitSwitchDeleteForce { git branch | % { $_.replace("*", "") } | % { $_.replace(" ", "") } | Invoke-Fzf | % { git branch -D $_ } }
 Set-Alias -Name fgdf -Value Invoke-FuzzyGitSwitchDeleteForce
 
 function Invoke-GitSwitch { git switch -c $args[0] }
@@ -44,3 +40,4 @@ Set-Alias -Name gap -Value Invoke-GitAddPartial
 
 function Invoke-ProfileUpdate { curl "https://raw.githubusercontent.com/markojerkic/.dotfiles/main/Microsoft.Powershell_profile.ps1" > $PROFILE; . $PROFILE }
 Set-Alias -Name up -Value Invoke-ProfileUpdate 
+
