83233654c33fc6f57e0720c816092d816f360134
Marko Jekrić <markojerkic266@gmail.com>
2023-11-23 17:42:36 +0100

    start new config

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
 cp ./.tmux.conf ~/.tmux.conf
 cp ./.gitconfig ~/.gitconfig
 cp ./.zshrc ~/.zshrc
+sudo ln -s ./update-nvim.sh /usr/local/bin/update-nvim
+sudo ln -s ./tmux-sessionizer.sh /usr/local/bin/tmux-sessionizer.sh
+
