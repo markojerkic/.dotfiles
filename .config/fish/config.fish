2f356dbc8616f8058b390f9bcb66256259fb35af
Marko Jerkić <markojerkic266@gmail.com>
2025-09-13 10:08:36 +0200

    cleanup

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -48,3 +48,6 @@ end
 set -gx PATH /home/marko/dev/.dotfiles/scripts $PATH
 
 set -x N_PREFIX "$HOME/n"; contains "$N_PREFIX/bin" $PATH; or set -a PATH "$N_PREFIX/bin"  # Added by n-install (see http://git.io/n-install-repo).
+
+# opencode
+fish_add_path /home/marko/.opencode/bin
